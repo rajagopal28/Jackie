675e2df8d158e9ad42324af43c2237bce3357cc6
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-08 19:41:38 +0530

    Adding query params parsing in get request

--- services/base-service.js
+++ services/base-service.js
@@ -1,8 +1,33 @@
 const BASE_URL = 'https://healthcare-server.herokuapp.com/';
+
 class BaseService {
   static getData(path, params, success, done) {
-    fetch(BASE_URL + path, {method: "GET"})
+    let queryString = '';
+    for(let key in params){
+      if(!params.hasOwnProperty(key)) continue;
+      if(queryString !== '') {
+        queryString += '&';
+      }
+      queryString += key + '='+params[key];
+    }
+    console.log(queryString.toString());
+    fetch(BASE_URL + path + '?' + queryString, {method: "GET"})
      .then((response) => response.json())
+     .then((response) => {
+       success(response);
+     }).done(() => {
+       done();
+     });
+  }
+  static postData(path, body, success, done) {
+    fetch(BASE_URL + path, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+     }).then((response) => response.json())
      .then((response) => {
        success(response);
      }).done(() => {

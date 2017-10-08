ba8f2675c3fcd384513fb278e860d70d734720e0
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-08 22:06:00 +0530

    Adding vital logs list view and item

---
+++ components/rows/vital-log-item.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+import ListViewStyles from '../../styles/list-view-styles';
+
+const styles = ListViewStyles;
+
+const VitalLogItemRow = (props) => (
+  <View style={styles.container}>
+    <Text style={styles.text}>
+      {`User: ${props.user.first_name} ${props.user.last_name}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Pressure: ${props.pressure}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Sugar: ${props.sugar}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Temperature: ${props.temperature}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Pulse: ${props.pulse}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Logged On: ${props.logged_on}`}
+    </Text>
+  </View>
+);
+
+export default VitalLogItemRow;
---
+++ components/vital-logs.js
@@ -0,0 +1,48 @@
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ListView
+} from 'react-native';
+import ListViewStyles from '../styles/list-view-styles';
+import UserService from '../services/user-service';
+import VitalLogItemRow from './rows/vital-log-item';
+
+const styles = ListViewStyles;
+class VitalLogScreen extends Component {
+  static navigationOptions = {
+    title: 'Out Patients',
+  };
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([]),
+    };
+  }
+  componentDidMount(){
+    const {state} = this.props.navigation;
+    console.log("Loading vitals of with... " + JSON.stringify(state.params));
+    this.loadJSONData(state.params);
+  }
+  loadJSONData(params) {
+    _this = this;
+    UserService.getVitalLogs(params, (dps) => {
+      _this.setState({ dataSource: this.state.dataSource.cloneWithRows(dps)});
+    });
+  }
+  render() {
+    return (
+      <ListView
+        contentContainerStyle={styles.list}
+        dataSource={this.state.dataSource}
+        renderRow={(da) => <VitalLogItemRow {...da} />}
+      />
+    );
+  }
+}
+export default VitalLogScreen;

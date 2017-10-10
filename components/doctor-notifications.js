bdd8c4f91ffce47eb361a3f3010897040f07e8e9
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-10 15:56:36 +0530

    Adding DoctorNotification screen, items, service call and everything

---
+++ components/doctor-notifications.js
@@ -0,0 +1,50 @@
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ListView
+} from 'react-native';
+
+import ListViewStyles from '../styles/list-view-styles';
+import DoctorService from '../services/doctor-service';
+import DoctorNotificationItemRow from './rows/doctor-notification-item';
+
+const styles = ListViewStyles;
+
+class DoctorNotificationScreen extends Component {
+  static navigationOptions = {
+    title: 'Doctor Appointments',
+  };
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([]),
+    };
+  }
+  componentDidMount(){
+    const {state} = this.props.navigation;
+    console.log("Loading Appointments with... " + JSON.stringify(state.params));
+    this.loadJSONData(state.params);
+  }
+  loadJSONData(params) {
+    _this = this;
+    DoctorService.getDoctorNotifications(params, (dps) => {
+      _this.setState({ dataSource: this.state.dataSource.cloneWithRows(dps)});
+    });
+  }
+  render() {
+    return (
+      <ListView
+        contentContainerStyle={styles.list}
+        dataSource={this.state.dataSource}
+        renderRow={(da) => <DoctorNotificationItemRow {...da} />}
+      />
+    );
+  }
+}
+export default DoctorNotificationScreen;
---
+++ components/rows/doctor-notification-item.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+import ListViewStyles from '../../styles/list-view-styles';
+
+const styles = ListViewStyles;
+
+const DoctorNotificationItemRow = (props) => (
+  <View style={styles.container}>
+    <Text style={styles.text}>
+      {`User: ${props.user.first_name} ${props.user.last_name}`}
+    </Text>
+    <Text style={styles.text}>
+      {`About: ${props.title}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Notified on: ${props.notified_on}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Severity: ${props.severity}`}
+    </Text>
+    <Text style={styles.text}>
+      {`Notes: ${props.notes}`}
+    </Text>
+  </View>
+);
+
+export default DoctorNotificationItemRow;

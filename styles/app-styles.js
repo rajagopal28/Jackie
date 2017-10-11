dafadac62a89aedb404a71359ee817b50fc5008d
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-11 20:22:15 +0530

    removing unused styles in home and adding row level styles list item

--- styles/app-styles.js
+++ styles/app-styles.js
@@ -6,7 +6,7 @@ const HomeStyles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
@@ -17,15 +17,6 @@ const HomeStyles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
-  form_container: {
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  form_input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1
   },
   formHeading : {fontSize: 20},
   formPadding: {padding: 20},
--- styles/list-view-styles.js
+++ styles/list-view-styles.js
@@ -4,21 +4,28 @@ import {
 
 
 const ListViewStyles = StyleSheet.create({
-   list: {
-          justifyContent: 'space-around',
-          flexWrap: 'wrap'
-      },
-  container: {
+  list: {
+      justifyContent: 'space-around',
+      flexWrap: 'wrap',
+      backgroundColor: '#EFF0F1'
+  },
+  row: {
+    padding: 10,
     flex: 1,
-    borderColor: 'gray',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    borderWidth: 1
+    borderBottomColor: '#bbb',
+    borderRadius:10,
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+    margin: 5
   },
   text: {
     marginLeft: 12,
-    fontSize: 16,
+    fontSize: 16,    // No background
+    fontFamily: 'Avenir',               // Change default font
+    // Add text shadow
+    textShadowColor: '#222',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 1,
   }
 });
 
